0a8691d7e238f0b24638a81dbb16e1820bba49a3
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-05-02 16:03:32 -0400

    added new pages

--- application/blueprints/index.py
+++ application/blueprints/index.py
@@ -5,3 +5,15 @@ bp = Blueprint("index", __name__, url_prefix="/")
 @bp.route("/", methods=("GET",))
 def home():
     return render_template("index.html")
+
+@bp.route("/student_registration", methods=("GET",))
+def student_reg():
+    return render_template("student_reg.html")
+
+@bp.route("/tutor_registration", methods=("GET",))
+def tutor_reg():
+    return render_template("tutor_reg.html")
+
+@bp.route("/login", methods=("GET",))
+def tutor_reg():
+    return render_template("login.html")
