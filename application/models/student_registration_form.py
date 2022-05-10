c22880a5e66511339851ce2799fe19975ec0a9b5
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2022-05-09 20:09:27 -0400

    completed the navbar
    added html and styling

--- application/models/student_registration_form.py
+++ application/models/student_registration_form.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, IntegerField, PasswordField, validators
 
+
 class StudentRegistrationForm(FlaskForm):
     student_name = StringField(validators=[validators.InputRequired()])
     parent_name = StringField(validators=[validators.InputRequired()])
     student_email = EmailField(validators=[validators.InputRequired(), validators.Email()])
     parent_email = EmailField(validators=[validators.InputRequired(), validators.Email()])
-    parent_phone_number = StringField(validators=[validators.Optional(), validators.Regexp(r"(?:\+\d{1,2})?\(?\d{3}\)?(?:-| *)?\d{3}(?:-| *)?\d{4}")])
+    parent_phone_number = StringField(
+        validators=[validators.Optional(), validators.Regexp(r"(?:\+\d{1,2})?\(?\d{3}\)?(?:-| *)?\d{3}(?:-| *)?\d{4}")])
     student_grade = IntegerField(validators=[validators.InputRequired()])
     student_school = StringField(validators=[validators.Optional()])
     student_timezone = StringField(validators=[validators.InputRequired()])
